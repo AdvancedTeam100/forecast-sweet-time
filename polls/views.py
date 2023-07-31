2091d6ddad346f0c6a1a70a5d0ac8034f446d128
AdvancedTeam100 <konadev0402@gmail.com>
2023-07-31 23:02:55 +0900

    change the views file to make an action of request

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from greytheory import GreyTheory
 
 # Create your views here.
@@ -7,24 +7,26 @@ from greytheory import GreyTheory
 grey = GreyTheory()
 
 def index(request):
-    gm11 = grey.gm11
+    if request.method == 'POST':
+        data = request.POST.getlist('array')
+        gm11 = grey.gm11
 
-    gm11.add_pattern(223.3, "a1")
-    gm11.add_pattern(227.3, "a2")
-    gm11.add_pattern(230.5, "a3")
-    gm11.add_pattern(238.1, "a4")
-    gm11.add_pattern(242.9, "a5")
-    gm11.add_pattern(251.1, "a6")
+        for value in data:
+            gm11.add_pattern(float(value), "")
 
-    gm11.period = 2 # Default is 1, the parameter means how many next moments need to forcast continually.
-    gm11.forecast()
-    #forecasted_results = gm11.forecasted_outputs()
-    #gm11.print_forecasted_results()
+        gm11.period = 2
+        gm11.forecast()
 
-    value_list = []
-    for value in gm11.analyzed_results:
-        value_list.append(value.forecast_value)
-    
-    print(value_list[-1])
-    return HttpResponse("Hello, world. You're at the polls index.")
+        value_list = []
+        for value in gm11.analyzed_results:
+            value_list.append(value.forecast_value)
+
+        result = {
+            'forecast_value': value_list[-1]
+        }
+
+        return JsonResponse(result)
+    else:
+        return HttpResponse("Invalid request method.")
+
 
