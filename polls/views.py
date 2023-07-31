4cf218183fe9f58612737ec184646e65b81be111
AdvancedTeam100 <konadev0402@gmail.com>
2023-08-01 01:30:31 +0900

    disable all actions

--- polls/views.py
+++ polls/views.py
@@ -11,32 +11,32 @@ grey = GreyTheory()
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
-        data = request.body.decode('utf-8')
-        print(data)
+        # data = request.body.decode('utf-8')
+        print(request)
         array = []
-        try:
-            json_data = json.loads(data)
-            array = json_data.get('array', [])
-        except json.JSONDecodeError as e:
-            print("Error decoding JSON:", str(e))
+        # try:
+        #     json_data = json.loads(data)
+        #     array = json_data.get('array', [])
+        # except json.JSONDecodeError as e:
+        #     print("Error decoding JSON:", str(e))
                 
-        gm11 = grey.gm11
+        # gm11 = grey.gm11
 
-        for value in array:
-            gm11.add_pattern(float(value), "")
+        # for value in array:
+        #     gm11.add_pattern(float(value), "")
 
-        gm11.period = 2
-        gm11.forecast()
+        # gm11.period = 2
+        # gm11.forecast()
 
-        value_list = []
-        for value in gm11.analyzed_results:
-            value_list.append(value.forecast_value)
+        # value_list = []
+        # for value in gm11.analyzed_results:
+        #     value_list.append(value.forecast_value)
 
-        result = {
-            'forecast_value': value_list[-1]
-        }
+        # result = {
+        #     'forecast_value': value_list[-1]
+        # }
 
-        return JsonResponse(result)
+        return JsonResponse("result")
     else:
         return HttpResponse("Invalid request method.")
 
